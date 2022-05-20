cdd1b797646b671c628df5528ed3f3c12ae0ac32
AndreyDidenkoStr <AndreyDidenkoStr@gmail.com>
2022-05-20 19:40:06 +0300

    Initial Commit

---
+++ MVVM-2/View/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  MVVM-2
+//
+//  Created by Andrey Kapitalov on 20.05.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+
+}
