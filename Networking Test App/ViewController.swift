b8029df20c080cb18b7942387d641e3aa5d32706
AndreyDidenkoStr <DidenkoAndreyStr@gmail.com>
2022-08-31 20:23:09 +0300

    GET and POST Requests

--- Networking Test App/ViewController.swift
+++ Networking Test App/ViewController.swift
@@ -29,8 +29,30 @@ class ViewController: UIViewController {
         }.resume()
     }
     
-    @IBAction func postResponse(_ sender: Any) {
+    @IBAction func postRquest(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        let userData = ["Course": "Networking", "Lesson": "Get and Post Request"]
         
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        guard let hhtpBody = try? JSONSerialization.data(withJSONObject: userData, options: []) else { return }
+        request.httpBody = hhtpBody
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            guard let response = response, let data = data else { return }
+            
+            print(response)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
 }
 
