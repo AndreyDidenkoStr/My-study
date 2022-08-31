aef77709e3f9bc062040caa61c04f8a4f19ce025
AndreyDidenkoStr <DidenkoAndreyStr@gmail.com>
2022-08-31 23:47:08 +0300

    new

---
+++ Networking Test App/CoursesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CoursesViewController.swift
+//  Networking Test App
+//
+//  Created by Andrey Kapitalov on 31.08.2022.
+//
+
+import UIKit
+
+class CoursesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
---
+++ Networking Test App/WebViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WebViewController.swift
+//  Networking Test App
+//
+//  Created by Andrey Kapitalov on 31.08.2022.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
